38f249e6b1ed6d632b741c36545089a4b4ef457e
Guilherme Cavalcanti <95859120+guipyc@users.noreply.github.com>
2025-04-04 20:54:19 +0000

    remoção de página de criação e transformação para modal

--- book-iventory/src/components/fab.tsx
+++ book-iventory/src/components/fab.tsx
@@ -1,14 +1,19 @@
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { ModalForm } from '@/components/form';
 
 export function FloatingButton() {
-	const navigate = useNavigate();
-	return (
-	  <button
-		onClick={() => navigate("/create")} 
-		className="fixed bottom-6 right-6 w-14 h-14 rounded-full bg-[var(--green-500)] text-white text-3xl shadow-md hover:bg-[var(--green-300)] transition-colors cursor-pointer "
-	  >
-		+
-	  </button>
-	);
-  }
-  
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={() => setOpen(true)}
+        className="fixed bottom-6 right-6 w-14 h-14 rounded-full bg-[var(--green-500)] text-white text-3xl shadow-md hover:bg-[var(--green-300)] transition-colors cursor-pointer"
+      >
+        +
+      </button>
+
+      <ModalForm isOpen={open} onClose={() => setOpen(false)} />
+    </>
+  );
+}
